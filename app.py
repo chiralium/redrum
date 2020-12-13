2f8b4542e754fb55263cf76056980b9261c5c4f5
Жека <be2e@yandex.ru>
2020-12-13 03:46:34 +0300

    Deploy test #4

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def deploy():
   if request_data is not None:
     if request_data.get('repository'):
       response = {
+        'checkout' : op.popen('git checkout .').read(),
         'fetch' : os.popen('git fetch').read(),
         'pull' : os.popen('git pull').read()
       }
